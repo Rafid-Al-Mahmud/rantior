235696d9577f27660a2928c23ff2fd197c4ded75
Rafid-Al-Mahmud <76583107+Rafid-Al-Mahmud@users.noreply.github.com>
2023-05-24 18:37:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+const Discord = require("discord.js");
+
+// Bot command to trigger the introduction process
+const INTRODUCTION_COMMAND = '/information';
+
+// Bot token here
+const TOKEN = 'MTEwOTUwODc2ODY0NDk5NzIwMg.GDJM1W.hoWC_lKX1gRsQSEJgOms6S_5Uk3MuxXluPicN0';
+
+// Channel ID where the introductions will be posted
+// const INTRODUCTION_CHANNEL_ID = 1109459105606795354;
+const INTRODUCTION_CHANNEL_ID = BigInt("1109592240566829127");
+
+// Questions to ask
+const questions = [
+    '➷ Name (Full):',
+    '➷ Surname:',
+    '➷ Age:',
+    // Add more questions here
+];
+
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.DirectMessages,
+        Discord.GatewayIntentBits.Guilds,
+        // Discord.GatewayIntentBits.GuildBans,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+    ]
+});
+
+module.exports = { Discord, client, TOKEN, INTRODUCTION_COMMAND, INTRODUCTION_CHANNEL_ID, questions };
+require('./clientEvents').c.login(TOKEN);
